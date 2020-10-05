cfcca233f5ec4970ae8bde58f7cd25d7b2c71c3a
Blacwin <magyarbalint91@gmail.com>
2020-10-05 14:36:27 +0200

    initialize

---
+++ src/services/auth.service.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const API_URL = "http://localhost:8080/oauth/token";
+
+class AuthService {
+    login(username, password) {
+        return axios
+            .post(API_URL, {username, password})
+            .then(response => {
+                if (response.data.accessToken) {
+                    localStorage.setItem("user", JSON.stringify(response.data));
+                }
+
+                return response.data;
+            });
+    }
+
+    logout() {
+        localStorage.removeItem("user");
+    }
+
+    getCurrentUser() {
+        return JSON.parse(localStorage.getItem('user'));
+    }
+}
+
+export default new AuthService();
